223cc1150b3492451b88cca4c269f5960765ae0c
Liz McCormack <lizmccormack@Lizs-MacBook-Pro-2.local>
2019-02-10 20:30:02 -0800

    module 1 printing exercise

---
+++ module_1_printing.py
@@ -0,0 +1,12 @@
+print "Balloonicorn is going to a party this weekend!"
+print 46
+print "It's a birthday party."
+print 22
+print "Balloonicorn needs to make a cake for her friend."
+print 49
+print "It will be red velvet."
+print 22
+print True
+print False
+print 54321
+print type(34)
