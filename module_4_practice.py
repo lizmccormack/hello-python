5e81e3dc145a0dab4a11325790be13d1730e5b4d
Elizabeth McCormack <elizabeth.mccormack@MK141-rMBP13.local>
2019-02-12 15:07:06 -0800

    add module 4 practice

---
+++ module_4_practice.py
@@ -0,0 +1,53 @@
+# 1
+characters = ['Chandler', 'Pheobe', 'Yoda', 'Spiderman', 'Jim']
+print (characters)
+# 2
+furniture = ['table', 'chairs', 'bed', 'desk']
+print (furniture)
+#3
+odd_nums = [3, 5, 7, 9, 11]
+print (odd_nums)
+#4
+even_nums = [2, 4, 6, 8, 10]
+print (even_nums)
+# 5
+todo_list = ["mail something", "run", "make dinner", "study for hackbright"]
+print (todo_list)
+# 6
+characters.append('Ross')
+print (characters)
+# 7
+furniture.append('dresser')
+print (furniture)
+# 8
+odd_nums.append(13)
+print (odd_nums)
+# 9
+even_nums.append(12)
+print (even_nums)
+# 10
+todo_list.append("sleep")
+print (todo_list)
+# 11
+sorted_nums = range(6)
+print (sorted_nums)
+# 12
+todo_list.pop()
+print (todo_list)
+# 13
+furniture.pop()
+print (furniture)
+# 14
+print (odd_nums[2])
+# 15
+print (even_nums[4])
+# 16
+word1 = 'delicious'
+print (word1[4])
+# 17
+word2 = 'yummy'
+print (word2[1])
+# 18
+some_numbers = [4, 2, 1, 6, 8]
+some_numbers.sort()
+print (some_numbers)
