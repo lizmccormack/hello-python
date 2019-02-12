7f3d0abfe8050c137f939c897577acffb31c9fea
Elizabeth McCormack <elizabeth.mccormack@MK141-rMBP13.local>
2019-02-12 12:00:21 -0800

    add changes

--- module_2_printing.py
+++ module_2_printing.py
@@ -7,14 +7,13 @@ family_member2_len = len(family_member2)
 family_member3_len = len(family_member3)
 family_member4_len = len(family_member4)
 
-print family_member1
-print family_member1_len
-print family_member2
-print family_member2_len
-print family_member3
-print family_member3_len
-print family_member4
-print family_member4_len
-next_family_member = input(Enter the name of a new family member:)
-print next_family_member
-print "Great, so you added %s" % next_family_member
+print (family_member1)
+print (family_member1_len)
+print (family_member2)
+print (family_member2_len)
+print (family_member3)
+print (family_member3_len)
+print (family_member4)
+print (family_member4_len)
+next_family_member = input("Enter the name of a new family member:")
+print ("Great, so you added %s" % next_family_member)
