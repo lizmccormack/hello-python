269c08b9e07f5e81a4529fd05a908fdc93e8e0e1
Elizabeth McCormack <elizabeth.mccormack@MK141-rMBP13.local>
2019-02-12 11:56:59 -0800

    add module 2 printing

---
+++ module_2_printing.py
@@ -0,0 +1,20 @@
+family_member1 = 'Reggie'
+family_member2 = 'Steve'
+family_member3 = 'Richie'
+family_member4 = 'Teddie'
+family_member1_len = len(family_member1)
+family_member2_len = len(family_member2)
+family_member3_len = len(family_member3)
+family_member4_len = len(family_member4)
+
+print family_member1
+print family_member1_len
+print family_member2
+print family_member2_len
+print family_member3
+print family_member3_len
+print family_member4
+print family_member4_len
+next_family_member = input(Enter the name of a new family member:)
+print next_family_member
+print "Great, so you added %s" % next_family_member
