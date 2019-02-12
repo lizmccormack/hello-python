cc856f4b2d511810bcda52203a1ab8a5834a3db6
Elizabeth McCormack <elizabeth.mccormack@MK141-rMBP13.local>
2019-02-12 14:12:25 -0800

    start user questions exercise

---
+++ module_3_user_questions.py
@@ -0,0 +1,46 @@
+
+cats = input("Do you like cats? Answer Y or N: ")
+
+if cats == "N":
+    not_cats = input("What is your favortie animal? ")
+else:
+    print ("Cats are cool!")
+
+chocolate = input("Do you like chocolate? Answer Y or N: ")
+
+if chocolate == "N":
+    not_chocolate = input("What is your favorite dessert? ")
+else:
+    print ("YUM!")
+
+coffee = input("Do you drink coffee? Answer Y or N: ")
+
+if coffee == "Y":
+    how_do_you_like_it = input("Do you put sugar in it? ")
+else:
+    what_do_you_drink = input("What is your favorite morning drink? ")
+
+cali = input("Do you live in California? Answer Y or N: ")
+
+if cali == 'Y':
+    city = input("What city do you live in? ")
+else:
+    state = input("What state do you live in? ")
+
+jokes = input ("Do you enjoy jokes? Answer Y or N: ")
+
+if jokes == "Y":
+    joke = input("What kinds of melon can't marry? " )
+    print ("Catelopes")
+else:
+    no_jokes = input("Why?")
+
+
+print (not_cats)
+print (not_chocolate)
+print (how_do_you_like_it)
+print (what_do_you_drink)
+print (city)
+print (state)
+print (joke)
+print (no_jokes)
