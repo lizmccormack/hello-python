836c2e79c3dab1d4630b4a3c423f5140073531d5
Elizabeth McCormack <elizabeth.mccormack@MK141-rMBP13.local>
2019-02-12 16:28:25 -0800

    add module 5 practice

---
+++ module_5_practice.py
@@ -0,0 +1,41 @@
+# 1
+counter = 1
+
+while counter < 8:
+    print (counter)
+    counter = counter + 1
+
+# 2
+for num in range(21):
+    print (num)
+
+# 3
+for num in range(11):
+    if num != 3:
+        if num != 5:
+            print (num)
+# 4
+counter = 100
+
+while counter > 0:
+    print (counter)
+    counter = counter - 1
+
+# 5
+
+
+
+# 6
+for num in range(80):
+    print (num)
+
+# 7
+colors = ["black", "orange", "purple"]
+for color in colors:
+    print (color)
+
+# 8
+for color in colors:
+    print (color[0])
+
+# 9
