9dfc2473d910f9f01d8fb399ad1f7f5579377bf3
Liz McCormack <lizmccormack@Lizs-MacBook-Pro-2.local>
2019-02-21 09:56:56 -0800

    add comments

--- module_6_printing.py
+++ module_6_printing.py
@@ -1,12 +1,15 @@
+# create a dictionary of cake ingredients
 cake_ingredients = {'butter': 1, 'shortening': 0.5, 'sugar': 3, \
 'egg substitutes': 1.5, 'flour': 3, 'milk': 1, 'salt': 0.01,\
 'baking powder': 0.01}
 
+# write a for loop that doubles the cake ingredients and prints the new amounts
 print ("To make two cakes, you need: ")
 for ing in cake_ingredients:
     doubled = cake_ingredients[ing]*2
     print(("%s cups of %s") % (doubled, ing))
 
+# ask the user if they have any questions
 question = input("What ingredient do you want to know about?")
 if question in cake_ingredients:
     print (("One cake has %s cups of %s") % (cake_ingredients[question], question))
