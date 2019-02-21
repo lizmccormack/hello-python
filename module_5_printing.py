3070e86b61cf6ea58b3f31250382e44f595bd446
Liz McCormack <lizmccormack@Lizs-MacBook-Pro-2.local>
2019-02-21 09:58:23 -0800

    add comments

--- module_5_printing.py
+++ module_5_printing.py
@@ -1,4 +1,4 @@
-# Part 1
+# Part 1: ask the user three quiz questions from a list of questions with a for loop
 quiz_questions = ['what are three datatypes? ', 'what are lists? ', 'what is the difference between print and return? ']
 
 for question in quiz_questions:
@@ -6,7 +6,7 @@ for question in quiz_questions:
     print (response)
 
 
-# Part 2
+# Part 2: create a todo list program with a while loop that ends when the user enters quit 
 todo_list = ["Do laundry"]
 
 command = input("would you like to A) add a todo list item or B) quit? ")
