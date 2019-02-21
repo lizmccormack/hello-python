0df54fbfe216d5fec74dd7e751a70588743a16c1
Liz McCormack <lizmccormack@Lizs-MacBook-Pro-2.local>
2019-02-21 10:07:52 -0800

    add comments

--- module_4_practice.py
+++ module_4_practice.py
@@ -1,53 +1,53 @@
-# 1
+# 1 print a list strings
 characters = ['Chandler', 'Pheobe', 'Yoda', 'Spiderman', 'Jim']
 print (characters)
-# 2
+# 2 print a list of strings
 furniture = ['table', 'chairs', 'bed', 'desk']
 print (furniture)
-#3
+#3 print a list of integers
 odd_nums = [3, 5, 7, 9, 11]
 print (odd_nums)
-#4
+#4 print a list of integers
 even_nums = [2, 4, 6, 8, 10]
 print (even_nums)
-# 5
+# 5 print a list of strings
 todo_list = ["mail something", "run", "make dinner", "study for hackbright"]
 print (todo_list)
-# 6
+# 6 add a strings to list of characters
 characters.append('Ross')
 print (characters)
-# 7
+# 7 add a piece of furniture to the list of furniture
 furniture.append('dresser')
 print (furniture)
-# 8
+# 8 add an integer to the list of odd numbers
 odd_nums.append(13)
 print (odd_nums)
-# 9
+# 9 add and integer to the list of even numbers
 even_nums.append(12)
 print (even_nums)
-# 10
+# 10 add a strings to the list of todo items
 todo_list.append("sleep")
 print (todo_list)
-# 11
+# 11 print a range of numbers from 0-5
 sorted_nums = range(6)
 print (sorted_nums)
-# 12
+# 12 remove the last item in the todo list
 todo_list.pop()
 print (todo_list)
-# 13
+# 13 remove the last item in the furniture list
 furniture.pop()
 print (furniture)
-# 14
+# 14 pront the odd number at index 2
 print (odd_nums[2])
-# 15
+# 15 print the even number at index 2
 print (even_nums[4])
-# 16
+# 16 print the character at the 4th index
 word1 = 'delicious'
 print (word1[4])
-# 17
+# 17 print the character at the first indyx
 word2 = 'yummy'
 print (word2[1])
-# 18
+# 18 sort numbers and print them 
 some_numbers = [4, 2, 1, 6, 8]
 some_numbers.sort()
 print (some_numbers)
