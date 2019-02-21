11fbe54bea44db2524974e20b6c52964b262153d
Liz McCormack <lizmccormack@Lizs-MacBook-Pro-2.local>
2019-02-21 09:50:45 -0800

    change while loop

--- module_5_printing.py
+++ module_5_printing.py
@@ -1,22 +1,19 @@
 # Part 1
-quiz_questions = ['what are three datatypes?', 'what are lists?', 'what is the difference between print and return']
+quiz_questions = ['what are three datatypes? ', 'what are lists? ', 'what is the difference between print and return? ']
 
 for question in quiz_questions:
-    input(question)
-    print (question) 
+    response = input(question)
+    print (response)
 
 
 # Part 2
 todo_list = ["Do laundry"]
 
-command = input("would you like to A) add a todo list item or B) quit ")
-while todo_list:
+command = input("would you like to A) add a todo list item or B) quit? ")
+while command != 'quit':
     if command == 'add':
         command = input('what would you like to add? ')
         todo_list.append(command)
-    elif command == 'quit':
-        break
-    else:
-        print('please enter add or quit')
+        command = input('what would you like to do next? Enter add or quit. ')
 
 print (todo_list)
